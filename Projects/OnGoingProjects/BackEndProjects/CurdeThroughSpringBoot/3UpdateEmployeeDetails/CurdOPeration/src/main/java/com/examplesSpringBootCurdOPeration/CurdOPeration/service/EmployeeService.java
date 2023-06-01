568c3ac6e7fbb4493f4a10f1cb819ffbc4ecf54a
Bharat Gayki <bhaaratgayki@gmail.com>
2023-06-01 10:40:21 +0530

    UpdateEmployeeDetails

---
+++ Projects/OnGoingProjects/BackEndProjects/CurdeThroughSpringBoot/3UpdateEmployeeDetails/CurdOPeration/src/main/java/com/examplesSpringBootCurdOPeration/CurdOPeration/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.examplesSpringBootCurdOPeration.CurdOPeration.service;
+
+import com.examplesSpringBootCurdOPeration.CurdOPeration.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+
+    Employee getEmployeeById(long id);
+}
